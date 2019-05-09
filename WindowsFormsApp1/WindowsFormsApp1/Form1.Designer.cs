0cc15afa31ebcaa76b1355e628e3f604d71acfdc
Ethan <ethanc@paragontruss.com>
2019-05-09 14:06:57 -0400

    Work
    work

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(34, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(938, 958);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Items.AddRange(new object[] {
+            "Circle",
+            "Rectangle"});
+            this.listBox1.Location = new System.Drawing.Point(999, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(545, 407);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1586, 1033);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,54 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int x;
+        int y;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point = new Point(e.X, e.Y);
+            x = point.X;
+            y = point.Y;
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            // Create graphics class
+            Graphics g = panel1.CreateGraphics();
+            // Set painting quality to high
+            e.Graphics.SmoothingMode =
+                SmoothingMode.HighQuality;
+
+            // Create a new pen class
+            Pen p = new Pen(Color.Black);
+
+            // If Circle is selected
+            if (listBox1.SelectedIndex == 0)
+            {
+                SolidBrush sb = new SolidBrush(Color.Red);
+                g.DrawEllipse(p, x - 50, y - 50, 100, 100);
+                g.FillEllipse(sb, x - 50, y - 50, 100, 100);
+            }
+
+            // If rectangle is selected
+            if (listBox1.SelectedIndex == 1)
+            {
+                SolidBrush sb = new SolidBrush(Color.Blue);
+                g.DrawRectangle(p, x- 50, y - 50, 100, 100);
+                g.FillRectangle(sb, x - 50, y - 50, 100, 100);
+                
+            }
         }
     }
 }
