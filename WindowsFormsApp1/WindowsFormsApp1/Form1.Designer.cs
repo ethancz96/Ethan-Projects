ba397b0efb0f1c9e86a0393de68e9f4ec24d0f27
Ethan <ethanc@paragontruss.com>
2019-05-09 14:57:55 -0400

    Work
    work

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,18 +47,28 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 31;
             this.listBox1.Items.AddRange(new object[] {
-            "Circle",
-            "Rectangle"});
+            "Circle"});
             this.listBox1.Location = new System.Drawing.Point(999, 39);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(545, 407);
             this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1029, 471);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 81);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1586, 1033);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -71,6 +82,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,10 +15,13 @@ namespace WindowsFormsApp1
     {
         int x;
         int y;
-
+        List<Point> points = new List<Point>();
+        List<Point> Rpoints = new List<Point>();
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -28,39 +31,60 @@ namespace WindowsFormsApp1
 
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
-            Point point = new Point(e.X, e.Y);
-            x = point.X;
-            y = point.Y;
+            points.Add(e.Location);
+            Rpoints.Add(e.Location);
             panel1.Invalidate();
         }
 
+        //private void panel1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        //    e.Graphics.FillEllipse(Brushes.Red, new Rectangle(10, 10, 32, 32));
+        //}
+
+        //private void panel1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    // Create graphics class
+        //    Graphics g = panel1.CreateGraphics();
+        //    // Set painting quality to high
+        //    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+        //    // Create a new pen class
+        //    Pen p = new Pen(Color.Black);
+
+        //    // If Circle is selected
+        //    if (listBox1.SelectedIndex == 0)
+        //    {
+        //        SolidBrush sb = new SolidBrush(Color.Red);
+        //        g.DrawEllipse(p, x - 50, y - 50, 100, 100);
+        //        g.FillEllipse(sb, x - 50, y - 50, 100, 100);
+        //    }
+
+        //    // If rectangle is selected
+        //    if (listBox1.SelectedIndex == 1)
+        //    {
+        //        SolidBrush sb = new SolidBrush(Color.Blue);
+        //        g.DrawRectangle(p, x - 50, y - 50, 100, 100);
+        //        g.FillRectangle(sb, x - 50, y - 50, 100, 100);
+
+        //    }
+        //}
+
+
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            // Create graphics class
-            Graphics g = panel1.CreateGraphics();
-            // Set painting quality to high
-            e.Graphics.SmoothingMode =
-                SmoothingMode.HighQuality;
+            g = e.Graphics;    // only ever use this one for persistent graphics!!
 
-            // Create a new pen class
-            Pen p = new Pen(Color.Black);
-
-            // If Circle is selected
             if (listBox1.SelectedIndex == 0)
             {
-                SolidBrush sb = new SolidBrush(Color.Red);
-                g.DrawEllipse(p, x - 50, y - 50, 100, 100);
-                g.FillEllipse(sb, x - 50, y - 50, 100, 100);
+                foreach (Point pt in points)
+                    g.FillEllipse(Brushes.Blue, pt.X, pt.Y, 50, 50);
             }
+        }
 
-            // If rectangle is selected
-            if (listBox1.SelectedIndex == 1)
-            {
-                SolidBrush sb = new SolidBrush(Color.Blue);
-                g.DrawRectangle(p, x- 50, y - 50, 100, 100);
-                g.FillRectangle(sb, x - 50, y - 50, 100, 100);
-                
-            }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            points.Clear();
         }
     }
 }
